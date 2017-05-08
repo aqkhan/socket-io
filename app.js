65a17136d13469e3dbd1625ffc4f5d6951faec3a
A Q Khan <aqk.neo@gmail.om>
2017-05-08 17:19:29 +0500

    Lift off

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+
+var http = require('http');
+
+var app = express();
+
+var server = http.createServer(app).listen(6900);
+
+var io = require('socket.io')(server);
+
+app.use(express.static('./public'));
+
+io.on('connection', function(socket){
+    socket.on('chat', function(message){
+        socket.broadcast.emit('message', message);
+    });
+
+    // On successful connection
+    socket.emit('message', 'Socket.io initialized');
+});
+
+console.log('Server listening to port 6900');
---
+++ public/ws-client.js
@@ -0,0 +1,49 @@
+// This is client side ws controller
+
+var socket = io('http://localhost:6900');
+
+socket.on('disconnect', function () {
+    updateTitle('Disconnected');
+});
+
+socket.on('connect', function () {
+    updateTitle('Connected');
+});
+
+socket.on('message', function (message) {
+    updateMSG(message);
+});
+
+function myKeyPress(e){
+    var keynum;
+
+    if(window.event) {                 
+      keynum = e.keyCode;
+    } else if(e.which){                   
+      keynum = e.which;
+    }
+
+    if(keynum == 13){
+        var msg = document.getElementById('msg').value;
+        updateMSG(msg);
+        socket.emit('chat', msg);
+    }
+  }
+
+function updateTitleMain(title){
+    document.getElementsByTagName('h1')[0].innerHTML = title;
+}
+
+function updateTitle(title){
+    document.getElementsByTagName('h2')[0].innerHTML = title;
+}
+
+function updateMSG(msg){
+    document.getElementById('msg').value = '';
+    var para = document.createElement("p");
+    var node = document.createTextNode('> ' + msg);
+    para.appendChild(node);
+
+    var element = document.getElementById("messages");
+    element.appendChild(para);
+}
